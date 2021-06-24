ff44764b5a3ded2a385f03103b7e42da76d446c3
MaganaOscar <oscmag13@gmail.com>
2021-06-24 16:05:19 -0700

    init commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let elem = document.getElementById("content");
+let user = null;
+async function getGit() {
+    user = document.getElementById("user").value;
+    var response = await fetch("https://api.github.com/users/" + user);
+    var coderData = await response.json();
+    elem.innerHTML = "<h1>" + coderData['name'] + " has " +
+    coderData['followers'] + " followers " + "</h1><img src='" + coderData['avatar_url']
+    + "' width='500' height='500'>"
+
+}
+
+console.log(getGit())
